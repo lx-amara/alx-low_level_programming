3a48fb69eb5515524961ab110c33ce5a532bb90f
lx-amara <faithzyswt@gmail.com>
2022-09-16 13:47:22 -0700

    functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - main block
- * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
- * Numbers must be coma and space separated.
- * Return: 0
- */
-
+  * main - Prints the first 52 fibonacci numbers
+  *
+  * Return: Nothing!
+  */
 int main(void)
 {
-    int count = 3; /* this is so because the first 2 members have been given already. My loop will therefore begin from the 3rd one */
+	int i = 0;
+	long j = 1, k = 2;
 
-    long int first = 1, second = 2;
-    long int next = first + second;
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
 
-    printf("%lu, ", first);
-    printf("%lu, ", second);
+		++i;
+	}
 
-    while (count <= 50)
-    {
-        /* Let's check if we are at the end of the list, if we are close with a new line */
-        if (count == 50)
-        {
-            printf("%lu \n", next);
-         }
-         else  /* if we are not at the end of the list, add a comma after the number */
-         { 
-           printf("%lu, ", next); 
-         }
-
-        /* Reset the variables to get the next number */
-        first = second;
-        second = next;
-
-        /* after resetting the variables, you need to find the next number */
-        next = first + second; 
-        count++;
-    }
-
-    return (0);
+	printf("\n");
+	return (0);
 }
-
